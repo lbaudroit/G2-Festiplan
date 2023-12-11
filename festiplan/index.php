7ac018ffe95b444329686a9d7e546f80d2168a8a
lbaudroit <109075845+lbaudroit@users.noreply.github.com>
2023-12-11 08:25:37 +0100

    Fontawesome

--- festiplan/index.php
+++ festiplan/index.php
@@ -4,7 +4,9 @@
 <head>
     <title>Accueil - Festiplan</title>
     <link rel="stylesheet" href="./libs/twbs/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="./libs/twbs/fortawesome/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -13,9 +15,7 @@
             <div class="col-12 text-center bg-success-subtle">
                 <h1>Coucou <i class="fa-regular fa-thumbs-up"></i></h1>
                 <?php
-                foreach (scandir("./libs/fortawesome/font-awesome") as $libs) {
-                    echo $libs . "<br>";
-                } ?>
+                echo "Coucou"; ?>
             </div>
         </div>
     </div>
