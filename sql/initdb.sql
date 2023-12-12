66400a6866154b140b527daef9ce53dbbc37636b
Tany CATALA BAILLY <catalatany@gmail.com>
2023-12-12 08:37:13 +0100

    init bd fini

--- sql/initdb.sql
+++ sql/initdb.sql
@@ -8,10 +8,16 @@ CREATE TABLE users (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE festivals (
-    'id_grij' INTEGER NOT NULL,
+    'id_festival' INTEGER NOT NULL,
     'date_deb' TIME NOT NULL,
     'date_fin' TIME NOT NULL,
     'temps_pause' TIME NOT NULL,
+    'id_grij' INTEGER NOT NULL,
+    'id_login' VARCHAR(35) NOT NULL,
+    'id_cat' INTEGER NOT NULL,
+    FOREIGN KEY (id_grij) REFERENCES grij(id_grij),
+    FOREIGN KEY (id_login) REFERENCES users(id_login),
+    FOREIGN KEY (id_cat) REFERENCES categoriefestival(id_cat),
     Primary key (id_grij)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -22,6 +28,10 @@ CREATE TABLE spectacles (
     'lien_img' VARCHAR(35),
     'duree' TIME NOT NULL,
     'categorie' VARCHAR(35),
+    'id_login' VARCHAR(35) NOT NULL,
+    'id_taille' INTEGER NOT NULL,
+    FOREIGN KEY (id_login) REFERENCES users(id_login),
+    FOREIGN KEY (id_taille) REFERENCES taillescene(id_taille),
     Primary key (id_spectacle),
     CONSTRAINT categorie CHECK (categorie IN ('concert', 'pièce de théatre', 'cirque', 'danse', 'projection de film'))
 );
@@ -38,6 +48,8 @@ CREATE TABLE scenes (
     'id_scene' INTEGER NOT NULL,
     'capacite' INTEGER NOT NULL,
     'GPS' POINT NOT NULL,
+    'id_taille' INTEGER NOT NULL,
+    FOREIGN KEY (id_taille) REFERENCES taillescene(id_taille),
     Primary key (id_scene)
 );
 
@@ -62,13 +74,25 @@ CREATE TABLE grij (
 );
 
 CREATE TABLE organise (
-
+    'id_festival' INTEGER NOT NULL,
+    'id_login' VARCHAR(35) NOT NULL,
+    FOREIGN KEY (id_login) REFERENCES users(id_login),
+    FOREIGN KEY (id_festival) REFERENCES festivals(id_festival),
+    PRIMARY KEY(id_festival, id_login)
 );
 
 CREATE TABLE estsurscene (
-
+    'id_intervenant' INTEGER NOT NULL,
+    'id_spectacle' INTEGER NOT NULL,
+    FOREIGN KEY (id_intervenant) REFERENCES intervenants(id_intervenant),
+    FOREIGN KEY (id_spectacle) REFERENCES spectacles(id_spectacle),
+    PRIMARY KEY(id_intervenant, id_spectacle)
 );
 
 CREATE TABLE esthorsscene (
-
+    'id_intervenant' INTEGER NOT NULL,
+    'id_spectacle' INTEGER NOT NULL,
+    FOREIGN KEY (id_intervenant) REFERENCES intervenants(id_intervenant),
+    FOREIGN KEY (id_spectacle) REFERENCES spectacles(id_spectacle),
+    PRIMARY KEY(id_intervenant, id_spectacle)
 );
