ad67320306452d252b9e46d3f8eca14ab1c50200
nael briot <nael.briot@iut-rodez.fr>
2023-12-13 10:29:45 +0100

    Ajout de la page de base

---
+++ festiplan/views/pageDefaut.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+		<title>FESTIPLAN - Login</title>
+		
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" rel="stylesheet" />
+    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"
+        rel="stylesheet">
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="..\css\style.css">
+    </head>
+    <body>
+        <header class="row fondbleu text-center">
+            <div class="col-1 titre">
+                <h3>Festiplan</h3>
+            </div>
+        </header>
+        <div class="fondblanc col-12">
+            bloups
+        </div>
+        <footer class ="row fondbleu">
+            <div class="offset-10">
+				<!--- Logo et lien IUT -->
+				Réalisé par <br/><a href="http://www.iut-rodez.fr" target="_blank"><img src="../images/logo-iut.png" class ="image" id="logoIUT" alt="Logo IUT" /></a>
+		    </div>
+        </footer>
+    </body>
+</html>
