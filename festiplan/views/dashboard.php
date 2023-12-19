8991abc635a6e39fb7a5cf723384eff0b359e7a5
Tany CATALA BAILLY <catalatany@gmail.com>
2023-12-19 10:28:23 +0100

    favicon dans dashboard

--- festiplan/views/dashboard.php
+++ festiplan/views/dashboard.php
@@ -22,7 +22,7 @@ if ($user!=null){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-
+    <link rel="icon" href="favicon.ico" />
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
