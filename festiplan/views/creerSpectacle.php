a8bd83e3d7862c013070e7fd944fc3932bf683e4
lbaudroit <109075845+lbaudroit@users.noreply.github.com>
2023-12-20 08:14:57 +0100

    Ajout du meta viewport

--- festiplan/views/creerSpectacle.php
+++ festiplan/views/creerSpectacle.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FESTIPLAN - Création de Spectacle</title>
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
--- festiplan/views/inscription.php
+++ festiplan/views/inscription.php
@@ -3,54 +3,60 @@
 
 <head>
     <title>Accueil - Festiplan</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"
         rel="stylesheet">
-    <link rel="stylesheet" href="../css/Inscription.css" > 
+    <link rel="stylesheet" href="../css/Inscription.css">
     <link rel="stylesheet" href="../css/style.css">
 </head>
-<body >
+
+<body>
     <header>
         <hspan class="titre">Festiplan</span>
     </header>
     <div class="container">
         <div class="row">
             <div class="col-md-12 page">
-				<div class="offset-2 col-8 offset-sm-2 col-sm-8 offset-md-4 col-md-4 cadre">
-					<img src="../images/compte_utilisateur_ombre.png">
-					<form>
-						<div class="offset-2 col-8 offset-sm-2 col-sm-6 offset-md-4 col-md-6 formulaire">
-							<label for="nom">nom:</label><br>
-							<input type="text" id="nom" name="nom"><br>
-							<label for="nom">prenom:</label><br>
-							<input type="text" id="prenom" name="prenom"><br>
-							<label for="identifiant">identifiant:</label><br>
-							<input type="text" id="identifiant" name="identifiant"><br>
-							<label for="motdepasse">mot de passe:</label><br>
-							<input type="text" id="motdepasse" name="motdepasse"><br>
-							<label for="email">email:</label><br>
-							<input type="text" id="email" name="email">
-						</div><br>
-						<div class="offset-3 col-6 offset-sm-3 col-sm-6 offset-md-3 col-md-6git status">
-							<button type="button" class="btn btn-primary btn-block"> Valider <i class="fa-solid fa-envelope"></i></button><br>
-							<a href="authentification.php" class="texte_lien">Se connecter</a><br>
-						</div>
-					</form>		
-				</div>
-			</div>
+                <div class="offset-2 col-8 offset-sm-2 col-sm-8 offset-md-4 col-md-4 cadre">
+                    <img src="../images/compte_utilisateur_ombre.png">
+                    <form>
+                        <div class="offset-2 col-8 offset-sm-2 col-sm-6 offset-md-4 col-md-6 formulaire">
+                            <label for="nom">nom:</label><br>
+                            <input type="text" id="nom" name="nom"><br>
+                            <label for="nom">prenom:</label><br>
+                            <input type="text" id="prenom" name="prenom"><br>
+                            <label for="identifiant">identifiant:</label><br>
+                            <input type="text" id="identifiant" name="identifiant"><br>
+                            <label for="motdepasse">mot de passe:</label><br>
+                            <input type="text" id="motdepasse" name="motdepasse"><br>
+                            <label for="email">email:</label><br>
+                            <input type="text" id="email" name="email">
+                        </div><br>
+                        <div class="offset-3 col-6 offset-sm-3 col-sm-6 offset-md-3 col-md-6git status">
+                            <button type="button" class="btn btn-primary btn-block"> Valider <i
+                                    class="fa-solid fa-envelope"></i></button><br>
+                            <a href="authentification.php" class="texte_lien">Se connecter</a><br>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
     <footer>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-6">
-                        <! ICI ON MET LE BOUTON DE DECONEXION>
-                    </div>
-                    <div class="col-6 contenue_droite">
-                        <img src="../images/logo-iut.png" class ="logo" id="logoIUT" alt="Logo IUT" href="http://www.iut-rodez.fr" target="_blank"/>
-                    </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6">
+                    <! ICI ON MET LE BOUTON DE DECONEXION>
+                </div>
+                <div class="col-6 contenue_droite">
+                    <img src="../images/logo-iut.png" class="logo" id="logoIUT" alt="Logo IUT"
+                        href="http://www.iut-rodez.fr" target="_blank" />
                 </div>
             </div>
-        </footer>
+        </div>
+    </footer>
 </body>
+
 </html>
