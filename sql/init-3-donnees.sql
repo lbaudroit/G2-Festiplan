6798e6a298d3a14925b576e0ce9a9f274b226dec
lbaudroit <109075845+lbaudroit@users.noreply.github.com>
2024-01-15 16:03:56 +0100

    Correction images

--- sql/init-3-donnees.sql
+++ sql/init-3-donnees.sql
@@ -32,19 +32,19 @@ VALUES
 -- Création des festivals
 INSERT INTO festivals (titre, description_f, lien_img, date_deb, date_fin, id_grij, id_login, id_cat)
 VALUES 
-("Premier de l\'an", "Un premier festival d\'exemple.", "f1.png", "2024-01-01", "2024-01-03", 1, "herverous", 1),
-("Saint Valentin", "Un deuxieme festival d\'exemple.", "f2.png", "2024-02-14", "2024-02-18", 2, "herverous", 2),
-("Pomme de terre 2024", "Un troisieme festival d\'exemple.", "f3.png", "2024-06-14", "2024-06-18", 3, "herverous", 3),
-("Natural Games 2024", "Un quatrieme festival d\'exemple.", "f4.png", "2024-09-26", "2024-09-29", 4, "herverous", 4);
+("Premier de l\'an", "Un premier festival d\'exemple.", ".png", "2024-01-01", "2024-01-03", 1, "herverous", 1),
+("Saint Valentin", "Un deuxieme festival d\'exemple.", ".png", "2024-02-14", "2024-02-18", 2, "herverous", 2),
+("Pomme de terre 2024", "Un troisieme festival d\'exemple.", ".png", "2024-06-14", "2024-06-18", 3, "herverous", 3),
+("Natural Games 2024", "Un quatrieme festival d\'exemple.", ".png", "2024-09-26", "2024-09-29", 4, "herverous", 4);
 
 -- Création de spectacles
 INSERT INTO spectacles (titre, description_s, lien_img, duree, id_cat, id_login, id_taille)
 VALUES 
-("JeChante", "Si vous aimez chanter alors vous chantez ou pas je sais pas.", "s1.png", "01:40:00", 1, "francksilvestre", 1),
-("JActe", "Des gens sur scène.", "s2.png", "01:40:00", 2, "francksilvestre", 2),
-("JeCirque", "Des clowns et des équilibristes (ou pas)", "s3.png", "01:40:00", 3, "francksilvestre", 2),
-("JeDanse", "Des gens qui bougent beaucoup mais c\'est beau.", "s4.png", "01:40:00", 4, "francksilvestre", 3),
-("JeProjette", "Tu t\'assoies et tu regardes...", "s5.png", "01:40:00", 5, "francksilvestre", 3);
+("JeChante", "Si vous aimez chanter alors vous chantez ou pas je sais pas.", ".png", "01:40:00", 1, "francksilvestre", 1),
+("JActe", "Des gens sur scène.", ".png", "01:40:00", 2, "francksilvestre", 2),
+("JeCirque", "Des clowns et des équilibristes (ou pas)", ".png", "01:40:00", 3, "francksilvestre", 2),
+("JeDanse", "Des gens qui bougent beaucoup mais c\'est beau.", ".png", "01:40:00", 4, "francksilvestre", 3),
+("JeProjette", "Tu t\'assoies et tu regardes...", ".png", "01:40:00", 5, "francksilvestre", 3);
 
 -- Création d'intervenants
 INSERT INTO intervenants (nom, prenom, email)
