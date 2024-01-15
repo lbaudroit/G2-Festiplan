7cf660c716ee4aa41bc49ea48c150dffbc48c98d
Tany CATALA BAILLY <catalatany@gmail.com>
2024-01-15 15:05:21 +0100

    ajout de trigger pour vider la plannif

--- sql/init-2-fonctions.sql
+++ sql/init-2-fonctions.sql
@@ -1,9 +1,17 @@
-DELIMITER //
-
 CREATE TRIGGER add_fest_owner_as_org 
 AFTER INSERT 
 ON festivals 
 FOR EACH ROW
 INSERT INTO organise (organise.id_festival, organise.id_login) VALUE (NEW.id_festival, NEW.id_login);
 
-DELIMITER ;
+CREATE TRIGGER sup_planif_on_insert
+AFTER INSERT 
+ON contient 
+FOR EACH ROW
+DELETE FROM planifie WHERE id_festival=NEW.id_festival;
+
+CREATE TRIGGER sup_planif_on_delete
+AFTER DELETE 
+ON contient 
+FOR EACH ROW
+DELETE FROM planifie WHERE id_festival=OLD.id_festival;
